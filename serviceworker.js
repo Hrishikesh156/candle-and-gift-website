c77d58ad0c461a095f544c4bbd94605b67ecf432
Hrishikesh kumbhar <hrishikumbhar156@gmail.com>
2023-03-02 13:37:43 +0530

    changes

--- serviceworker.js
+++ serviceworker.js
@@ -1,76 +1,82 @@
 self.addEventListener("install", function (event) {
-    event.waitUntil(preLoad());
-  });
- 
-  self.addEventListener("fetch", function (event) {
-    event.respondWith(
-      checkResponse(event.request).catch(function () {
-        console.log("Fetch from cache successful!");
-        return returnFromCache(event.request);
-      })
-    );
-    console.log("Fetch successful!");
-    event.waitUntil(addToCache(event.request));
-  });
-  self.addEventListener("sync", (event) => {
-    if (event.tag === "syncMessage") {
-      console.log("Sync successful!");
+  event.waitUntil(preLoad());
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    checkResponse(event.request).catch(function () {
+      console.log("Fetch from cache successful!");
+      return returnFromCache(event.request);
+    })
+  );
+  console.log("Fetch successful!");
+  event.waitUntil(addToCache(event.request));
+});
+
+self.addEventListener("push", function (event) {
+  if (event && event.data) {
+    var data = event.data.json();
+    if (data.method == "pushMessage") {
+      console.log("Push notification sent");
+      event.waitUntil(
+        self.registration.showNotification("Candles and Gifts | Buy Handmade Candles and Unique Gifts Online ", {
+          body: data.message,
+          icon: "./icons/icon96.png",
+        })
+      );
     }
+  }
+});
+
+
+self.addEventListener("sync", (event) => {
+  if (event.tag === "syncMessage") {
+    console.log("Sync successful!");
+  }
+});
+
+
+
+
+var filesToCache = ["/index.html"];
+
+var preLoad = function () {
+  return caches.open("offline").then(function (cache) {
+    // caching index and important routes
+    return cache.addAll(filesToCache);
   });
- 
-  self.addEventListener("push", function (event) {
-    if (event && event.data) {
-      var data = event.data.json();
-      if (data.method == "pushMessage") {
-        console.log("Push notification sent");
-        event.waitUntil(
-          self.registration.showNotification("Candles and Gifts | Buy Handmade Candles and Unique Gifts Online ", {
-            body: data.message,
-            icon: "./icons/icon96.png",
-          })
-        );
+};
+
+var checkResponse = function (request) {
+  return new Promise(function (fulfill, reject) {
+    fetch(request).then(function (response) {
+      if (response.status !== 404) {
+        fulfill(response);
+      } else {
+        reject();
       }
-    }
+    }, reject);
   });
+};
 
-
-
-
-  var filesToCache = ["/index.html"];
- 
-  var preLoad = function () {
-    return caches.open("offline").then(function (cache) {
-      // caching index and important routes
-      return cache.addAll(filesToCache);
+var addToCache = function (request) {
+  return caches.open("offline").then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
     });
-  };
- 
-  var checkResponse = function (request) {
-    return new Promise(function (fulfill, reject) {
-      fetch(request).then(function (response) {
-        if (response.status !== 404) {
-          fulfill(response);
-        } else {
-          reject();
-        }
-      }, reject);
+  });
+};
+
+var returnFromCache = function (request) {
+  return caches.open("offline").then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status == 404) {
+        return cache.match("offline.html");
+      } else {
+        return matching;
+      }
     });
-  };
- 
-  var addToCache = function (request) {
-    return caches.open("offline").then(function (cache) {
-      return fetch(request).then(function (response) {
-        return cache.put(request, response);
-      });
-    });
-  };
- 
-  var returnFromCache = function (request) {
-    const cache =  caches.open("offline");
-    const matching =  cache.match(request);
-    if (!matching || matching.status == 404) {
-      return cache.match("offline.html");
-    } else {
-      return matching;
-    }
-  };
+  });
+};
+
+
